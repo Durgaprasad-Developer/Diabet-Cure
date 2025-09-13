0647b00c121dfaa675ce7e6b0d0530ac803eb38e
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-09-14 05:29:58 +0530

    refactor: remove test route from user routes

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -5,9 +5,6 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const route = express.Router();
 route.post("/profile", authMiddleware, createOrUpdateProfile);
-route.get("/test", (req, res) => {
-  res.send("Profile routes working ✅");
-});
 
 
 export default route;
