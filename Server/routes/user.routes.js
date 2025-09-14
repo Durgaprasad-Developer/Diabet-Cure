76a5ae4182df8aee3c2c63f692c8f8a9dacd9d50
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-09-14 06:20:44 +0530

    feat: implement user profile creation and update functionality

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createOrUpdateProfile } from "../controllers/profile.controllers.js"
+import { createProfile, editOrUpdateProfile, getCurrentUser } from "../controllers/user.controllers.js"
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 
 const route = express.Router();
-route.post("/profile", authMiddleware, createOrUpdateProfile);
+route.post("/profile", authMiddleware, createProfile);
+route.get("/current", authMiddleware, getCurrentUser);
+route.put("/profileupdate", authMiddleware, editOrUpdateProfile)
 
 
 export default route;
