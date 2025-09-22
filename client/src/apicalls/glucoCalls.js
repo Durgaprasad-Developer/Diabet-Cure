3417c3681bf7b300c648f60ca9d4b525791786ad
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-09-22 11:58:28 +0530

    successful: seting up gluco endpoints from client to server

--- client/src/apicalls/glucoCalls.js
+++ client/src/apicalls/glucoCalls.js
@@ -14,6 +14,36 @@ export const addGlucose = async({value, mealContext,mealTag, notes,}) =>{
     console.log("add Glucose respone: ", response.data)
     return response.data
 }catch(err){
-    console.error("add Glucose error", err);
+    console.error("add Glucose error: ", err);
 }
+}
+
+export const getGlucose = async() => {
+    try{
+        const response = await api.get(`/api/glucose`)
+        console.log("get Glucose response", response.data);
+        return response.data
+    }catch(err){
+        console.error("get Glucose error: " err)
+    }
+}
+
+export const getGlucoseAverages = async() =>{
+    try{
+        const response = await api.get(`/api/glucose/averages`);
+        console.log("get Glucose averages: ", response.data);
+        return response.data;
+    }catch(err){
+        console.error("get Glucose averages error: ", err);
+    }
+}
+
+export const getGlucoseSummary = async() =>{
+    try{
+        const response = await api.get(`/api/glucose/summary`);
+        console.log("get Glucose Summary: ", response.data);
+        return response.data;
+    }catch(err){
+        console.error("get Glucose Summary error: ", err);
+    }
 }
