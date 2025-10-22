f742c7d7da8789b574356830c794314037fea251
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-10-23 01:29:54 +0530

    fixing issues for the production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-bro when I am refresing it goes to index so if there is token then it will direct to this page import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Signin from "./pages/Signin.jsx";
@@ -11,51 +11,50 @@ import { Navigate } from "react-router-dom";
 import Glucosereadings from "./pages/Glucosereadings.jsx";
 import AIReport from "./pages/AIReport.jsx";
 import Profile from "./pages/Profile.jsx";
-
 function App() {
   useCurrentUser();
   const userData = useSelector((state) => state.user.user);
   console.log("App.jsx", userData);
   return (
     <Routes>
+      {" "}
       <Route
         path="/"
         element={userData ? <Dashboard /> : <Navigate to="/landingpage" />}
-      />
+      />{" "}
       <Route
         path="/signup"
         element={!userData ? <Signup /> : <Navigate to="/" />}
-      />
-      {/* <Route path="/signin" element={<Signin/>}/> */}
+      />{" "}
+      {/* <Route path="/signin" element={<Signin/>}/> */}{" "}
       <Route
         path="/signin"
         element={!userData ? <Signin /> : <Navigate to="/" />}
-      />
+      />{" "}
       <Route
         path="/landingpage"
         element={!userData ? <Landingpage /> : <Navigate to="/" />}
-      />
+      />{" "}
       <Route
         path="/forgot-password"
         element={!userData ? <Forgotpassword /> : <Navigate to="/" />}
-      />
-      <Route path="/profileSetup" element={<ProfileSetup />} />
+      />{" "}
+      <Route path="/profileSetup" element={<ProfileSetup />} />{" "}
       <Route
         path="/glucose-readings"
         element={
           userData ? <Glucosereadings /> : <Navigate to="/landingpage" />
         }
-      />
+      />{" "}
       <Route
         path="/aiReport"
         element={userData ? <AIReport /> : <Navigate to="/landingpage" />}
-      />
+      />{" "}
       <Route
         path="/profile"
         element={userData ? <Profile /> : <Navigate to="/landingpage" />}
-      />
+      />{" "}
     </Routes>
   );
 }
-
 export default App;
